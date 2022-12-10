8d19d9a1467af334c755b335e29a7c54aaac202d
Beshut <105577734+Beshut@users.noreply.github.com>
2022-12-10 10:51:35 +0300

    Create Program3.cs

---
+++ Program3.cs
@@ -0,0 +1,12 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age = 27;//обьявление и присвоение значения переменной
+        Console.WriteLine(String.Format("Age: {0}", age));//вывод на экран сообщения пользователю
+        Console.WriteLine("");//пустая строка
+        Console.WriteLine($"Age: {age}");//вывод на экран сообщения пользователю
+        Console.WriteLine("");//пустая строка
+    }
+}
