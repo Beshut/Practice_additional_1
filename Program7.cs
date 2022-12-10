59ea3abf712fce38b746d7be1cfc3431211effe4
Beshut <105577734+Beshut@users.noreply.github.com>
2022-12-10 10:55:14 +0300

    Create Program7.cs

---
+++ Program7.cs
@@ -0,0 +1,9 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Remove demo1: " + "Hello".Remove(2));//удаляет подстроку из исходной строки
+        Console.WriteLine("Remove demo2: " + "Hello".Remove(2, 2));//удаляет подстроку из исходной строки
+    }
+}
