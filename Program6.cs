807b5cc4f618331edd5176e8937d24f2e36bc394
Beshut <105577734+Beshut@users.noreply.github.com>
2022-12-10 10:53:12 +0300

    Create Program6.cs

---
+++ Program6.cs
@@ -0,0 +1,9 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s12 = "Hello";//обьявление и заполнение переменной типа string
+        Console.WriteLine("Get element by index s12[3]: " + s12[3]);//вывод на экран сообщение пользователю, обьединение строк
+    }
+}
