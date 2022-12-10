99fc4ff4260da8267a6d55d5a0ff76ce61340085
Beshut <105577734+Beshut@users.noreply.github.com>
2022-12-10 10:51:02 +0300

    Create Program2.cs

---
+++ Program2.cs
@@ -0,0 +1,11 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("first line\nSecond line");//вывод сообщения на экран пользователю, переход на новую строку
+        Console.WriteLine(@"first line\nSecond line");//вывод сообщения на экран пользователю, отмена переход на новую строку
+    }
+}
+Footer
+© 2022 GitHub, Inc.
