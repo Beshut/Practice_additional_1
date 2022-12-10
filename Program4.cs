b468449f89555ad39a805ca0ed24c136fb19c086
Beshut <105577734+Beshut@users.noreply.github.com>
2022-12-10 10:52:07 +0300

    Create Program4.cs

---
+++ Program4.cs
@@ -0,0 +1,10 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        char[] charArray = { 'H', 'e', 'l', 'l', 'o' };//обьявление и заполнение массива
+        string s8 = new string(charArray);//"Hello"
+        string s9 = new string('O', 10);//"OOOOOOOOOO"
+    }
+}
